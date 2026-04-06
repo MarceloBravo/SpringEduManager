56f13a6af8d2bc9192593ddf450ac4b20111e094
Marcelo Bravo <mabc@live.cl>
2026-04-06 16:05:42 -0400

    Fix: Se eliminaron importaciones innecesarias y código comentado de los servicios EstudianteServiceImpl.java y UserServiceImpl.java

--- src/main/java/com/SpringEduManager/web/services/usuarios/UserServiceImpl.java
+++ src/main/java/com/SpringEduManager/web/services/usuarios/UserServiceImpl.java
@@ -146,17 +146,6 @@ public class UserServiceImpl implements UserService {
      * @throws RuntimeException si el email ya está registrado por otro usuario
      */
     private void validaEmail(UserDTO _user){
-        /*
-        // Regex estándar para validación de email
-        String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
-        
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(_user.getEmail());
-        
-        if(!matcher.matches()){
-            throw new RuntimeException("El email no es válido.");
-        }
-         */
         Usuario isEmailExists = this.repository.findByEmail(_user.getEmail()).orElse(null);
         if(isEmailExists != null && isEmailExists.getId() != _user.getId()){
             throw new RuntimeException("El email ya está registrado.");
